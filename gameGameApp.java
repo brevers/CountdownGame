3fd4a17d9175ac9796356cb12c8e65c7537aae4d
TamTamDB <45858960+brevers@users.noreply.github.com>
2021-06-05 23:28:53 +0100

    Update gameGameApp.java

--- gameGameApp.java
+++ gameGameApp.java
@@ -13,17 +13,16 @@ public class gameGameApp {
     public static void main(String[] args) {
         // variables
 
-        int rounds;
         String wordP1;
         String wordP2;
         int playAgain;
-
+        int rounds;
         //object
         Game myGame = new Game();
 
 
         //input
-        do{ //play again do while
+        do{ //play again 
         rounds = Integer.parseInt(JOptionPane.showInputDialog(null,"How many rounds do you want to play?"));
 
 
@@ -32,12 +31,12 @@ public class gameGameApp {
             myGame.computeGame();
 
             //insert word from Player 1
-            wordP1 = JOptionPane.showInputDialog(null,"Player 1 "+ "\n"+ "Insert your word: ");
+            wordP1 = JOptionPane.showInputDialog(null,"Player 1 " + "\n"+ "Insert your word: ");
             myGame.setWordP1(wordP1);
 
 
             //insert word from Player 2
-            wordP2 = JOptionPane.showInputDialog(null,"Player 2 "+ "\n"+ "Insert your word: ");
+            wordP2 = JOptionPane.showInputDialog(null,"Player 2 " + "\n"+ "Insert your word: ");
             myGame.setWordP2(wordP2);
 
             myGame.computeCheck();
@@ -49,11 +48,12 @@ public class gameGameApp {
         JOptionPane.showMessageDialog(null, "Random letters "+ myGame.getTotalRanLetters() + "\n" +"Player1 word: " +myGame.getWordP1() + "\n" + "Player2 word: " + myGame.getWordP2()+
                 "\n+"+"\n+"+"Total Score Player 1: "+ myGame.getScoreP1()+"points"+ "\n+"+"Total Score Player 2: "+ myGame.getScoreP2()+"points");
         }
+            
         //output Total end game scores
 
-        if (myGame.getScoreP1()> myGame.getScoreP2()){
+        if(myGame.getScoreP1() > myGame.getScoreP2()){
             JOptionPane.showMessageDialog(null, "The final winner is Player 1");
-        }else if(myGame.getScoreP1()< myGame.getScoreP2()){
+        }else if(myGame.getScoreP1() < myGame.getScoreP2()){
             JOptionPane.showMessageDialog(null, "The final winner is Player 2");
         }else{
             JOptionPane.showMessageDialog(null,  "\n" +"FINAL SCORE: It's a draw");
@@ -62,7 +62,7 @@ public class gameGameApp {
 
         //Play again Game
         playAgain = JOptionPane.showConfirmDialog(null, " Do you want to play again? ", " Notice ", JOptionPane.YES_NO_OPTION);
-        }while (playAgain== JOptionPane.YES_OPTION);
+        }while (playAgain == JOptionPane.YES_OPTION);
 
 
             }
